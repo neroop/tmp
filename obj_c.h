e4f282b11d7d82f36991ce41c8ac57c512370c75
neroop <43434133+neroop@users.noreply.github.com>
2024-06-13 00:16:50 +0800

    Create obj_c.h

---
+++ obj_c.h
@@ -0,0 +1,14 @@
+//
+// Created by win on 2024/6/12.
+//
+
+#ifndef BIND_OBJ_WRAPPER_H
+#define BIND_OBJ_WRAPPER_H
+
+struct Demo;
+
+struct Demo* constructDemo();
+void destroyDemo(struct Demo* demo);
+
+
+#endif //BIND_OBJ_WRAPPER_H
